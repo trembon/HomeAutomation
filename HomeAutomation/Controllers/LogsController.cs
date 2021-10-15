d3afcc9520fb01e2cd2835fad06026484e851c48
trembon <trembon@hotmail.com>
2021-10-15 22:43:38 +0200

    basic view of db stored logs

--- HomeAutomation/Controllers/LogsController.cs
+++ HomeAutomation/Controllers/LogsController.cs
@@ -2,15 +2,29 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using HomeAutomation.Database;
 using Microsoft.AspNetCore.Mvc;
 
 namespace HomeAutomation.Controllers
 {
     public class LogsController : Controller
     {
+        private readonly LogContext logContext;
+
+        public LogsController(LogContext logContext)
+        {
+            this.logContext = logContext;
+        }
+
         public IActionResult Index()
         {
             return View();
+        }
+
+        public IActionResult Stored()
+        {
+            var logs = logContext.Rows.OrderByDescending(x => x.Timestamp).Take(300).ToList();
+            return View(logs);
         }
 
         public IActionResult SystemStream()
