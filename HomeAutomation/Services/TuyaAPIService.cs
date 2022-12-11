6d6ab593c7898649871cb4e3fc0c41f2dd6e5dbe
trembon <trembon@hotmail.com>
2022-12-11 21:55:33 +0100

    fix color dps calculation

--- HomeAutomation/Services/TuyaAPIService.cs
+++ HomeAutomation/Services/TuyaAPIService.cs
@@ -66,12 +66,39 @@ namespace HomeAutomation.Services
                     if (state == DeviceState.On)
                     {
                         result[20] = true;
-                        result[22] = 1000; // brightness: 10-1000 (1-100%)
-                        //result["23"] = 1000; // temperature: 0-1000 (0-100%)
+
+                        // brightness: 10-1000 (1-100%)
+                        // if not specified, always go for 100%
+                        if (parameters.TryGetValue("brightness", out string brightness))
+                        {
+                            result[22] = int.Parse(brightness) * 10;
+                        }
+                        else
+                        {
+                            result[22] = 1000;
+                        }
+
+                        // temperature: 0-1000 (0-100%)
+                        // if not specified, always go for 100%
+                        if (parameters.TryGetValue("temperature", out string temperature))
+                        {
+                            result[22] = int.Parse(temperature) * 10;
+                        }
+                        else
+                        {
+                            result[22] = 1000;
+                        }
+
+                        // color: HSV value of the wanted color in hex values
+                        // hue 0 - 360, saturation 0 - 360, value 0 - 1000
+                        // ex: 00DC004B004E > 00DC|004B|004E > hue|saturation|value
                         if (parameters.TryGetValue("color", out string hexColor))
                         {
-                            result[21] = "color";
+                            result[21] = "colour";
                             result[24] = ColorTranslator.FromHtml(hexColor).ToHSVString();
+
+                            // always go for 100% brightness when color is set, as this will change the color
+                            result[23] = 1000;
                         }
                         else
                         {
