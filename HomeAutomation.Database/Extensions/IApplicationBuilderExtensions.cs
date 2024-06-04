5dc32e605b6f29f40bcd009913a7ab46a27877b6
trembon <trembon@hotmail.com>
2024-06-04 11:29:47 +0200

    fix namespce style

--- HomeAutomation.Database/Extensions/IApplicationBuilderExtensions.cs
+++ HomeAutomation.Database/Extensions/IApplicationBuilderExtensions.cs
@@ -3,21 +3,20 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 
-namespace HomeAutomation.Database.Extensions
-{
-    public static class IApplicationBuilderExtensions
-    {
-        public static void ApplyDatabaseMigrations(this IApplicationBuilder host)
-        {
-            host.ApplyDatabaseMigration<DefaultContext>();
-            host.ApplyDatabaseMigration<LogContext>();
-        }
+namespace HomeAutomation.Database.Extensions;
 
-        private static void ApplyDatabaseMigration<TContext>(this IApplicationBuilder host) where TContext : DbContext
-        {
-            using var scope = host.ApplicationServices.CreateScope();
-            var context = scope.ServiceProvider.GetRequiredService<TContext>();
-            context.Database.Migrate();
-        }
+public static class IApplicationBuilderExtensions
+{
+    public static void ApplyDatabaseMigrations(this IApplicationBuilder host)
+    {
+        host.ApplyDatabaseMigration<DefaultContext>();
+        host.ApplyDatabaseMigration<LogContext>();
+    }
+
+    private static void ApplyDatabaseMigration<TContext>(this IApplicationBuilder host) where TContext : DbContext
+    {
+        using var scope = host.ApplicationServices.CreateScope();
+        var context = scope.ServiceProvider.GetRequiredService<TContext>();
+        context.Database.Migrate();
     }
 }
