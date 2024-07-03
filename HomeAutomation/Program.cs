a8f6e36f0492b0c29189b749b84043c529ba290a
trembon <trembon@hotmail.com>
2024-07-04 00:04:43 +0200

    added mudblazor

--- HomeAutomation/Program.cs
+++ HomeAutomation/Program.cs
@@ -4,6 +4,7 @@ using HomeAutomation.Database.Extensions;
 using HomeAutomation.ScheduledJobs;
 using HomeAutomation.Core.Extensions;
 using HomeAutomation.Core.Services;
+using MudBlazor.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -12,14 +13,19 @@ builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents()
     .AddInteractiveWebAssemblyComponents();
 
+builder.Services.AddMudServices();
+
 builder.Services.AddDefaultDatabaseContext(builder.Configuration.GetConnectionString("Default")!);
 builder.Services.AddLoggingDatabaseContext(builder.Configuration.GetConnectionString("Logging")!);
 
+builder.Services.AddSingleton<ITelldusAPIService, TelldusAPIService>();
 builder.Services.AddSingleton<IJsonDatabaseService, JsonDatabaseService>();
+builder.Services.AddSingleton<IEvaluateConditionService, EvaluateConditionService>();
 
 builder.Services.AddTransient<ISunDataService, SunDataService>();
 builder.Services.AddTransient<ITriggerService, TriggerService>();
 builder.Services.AddTransient<INotificationService, NotificationService>();
+builder.Services.AddTransient<IActionExecutionService, ActionExecutionService>();
 
 builder.Services.AddScheduleJob<CleanupLogScheduleJob>();
 builder.Services.AddScheduleJob<ImportSunDataScheduleJob>();
